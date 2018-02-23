407bd46884d70a8cd1f6b907ca173c7e8f22db75
jagvenug <jagadeesh.venugopal@capgemini.com>
2018-02-23 14:31:43 +0530

    Update foo.r

--- foo.r
+++ foo.r
@@ -1,4 +1,6 @@
- #assignment
+# clear memory 
+rm(list = ls())
+#assignment
  1+2 
  s <- 40
  area <- s * s
@@ -8,3 +10,36 @@
  ls()
  #revove a var from memory 
  rm(area)
+
+
+# automic var
+TRUE
+class (TRUE)
+FALSE 
+class( FALSE)
+class (NA)
+
+# nummeric
+2.3
+2
+class(2)
+#int
+2L
+class(2L)
+
+#char 
+class ( "helllo there")
+
+
+#automic data types
+#double - higher precision 
+#complex - complex numbers
+#raw - store raw types
+
+# type conversion
+#Coercion
+as.numeric( TRUE)
+
+as.integer(4.5)
+#[1] 4
+
